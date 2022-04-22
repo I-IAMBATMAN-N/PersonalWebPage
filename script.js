bb9b703daed5052a3bc5024f3882e0b378e804e3
I-IAMBATMAN-N <jakub.briza.1992@seznam.cz>
2022-04-22 16:49:59 +0200

    contact.html - load

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const container = document.querySelector(".container");
 
 let views = document.querySelectorAll('.view');
 
+let inputs = document.querySelectorAll('input');
+
 let viewDescriptions = document.querySelectorAll('.viewDescription');
 
 let timer = 2000;
@@ -31,6 +33,36 @@ for(let i = 0; i < views.length; i++){
     timer);
     timer -= 300;
 }
+/*loading CONTACT FORM contact.html*/
+let projectTimer = 2000;
+
+for(let i = 0; i < inputs.length; i++){
+    setTimeout(() => {
+        inputs[i].style.opacity = "100";
+    },
+    projectTimer);
+    projectTimer -= 250;
+}
+
+let contactLinks = document.querySelectorAll('.contactLink');
+let contactLinksTimer = 1234;
+
+for(let i = 0; i < contactLinks.length; i++){
+    setTimeout(() => {
+        contactLinks[i].style.opacity = "100";
+    },
+    contactLinksTimer);
+    contactLinksTimer -= 200;
+}
+
+setTimeout(() => {
+   document.querySelector('textarea').style.opacity = "100";
+},
+2400);
+setTimeout(() => {
+    document.querySelector('button').style.opacity = "100";
+ },
+ 2200);
 
 /*hamburger animatiuon handler*/
 hamburger.addEventListener("click", () => {
