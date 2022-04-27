efb6a299d65810ce3a6ef391b946f06a1154eb4e
I-IAMBATMAN-N <jakub.briza.1992@seznam.cz>
2022-04-27 20:13:12 +0200

    summary
    projects.html/EN/CZE
    - adding projects with GitHub links
    
    projects.css
    - adjusting css accordingly
    
    script.js
    - adjusting script.js for added projects animations
    
    - fixing issue with container loading after activating "burger" menu
    
    contact.css
    -fixing bad text visibility by adjusting background color of light elements
     - makes better contrast

--- script.js
+++ script.js
@@ -7,7 +7,18 @@ const container = document.querySelector('.container');
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    container.classList.toggle("active-hide")
+
+    if(navMenu.getBoundingClientRect().x !== 0){
+        // container.style.display = "none";
+        container.style.opacity = "0";
+        container.style.transition = "0s";
+    }else if(navMenu.getBoundingClientRect().x == 0){
+        setTimeout(() => {
+            container.style.opacity = "100%";
+            container.style.transition = "2s";
+        },
+        400);
+    }
 })
 
 /*=============================LOAD functions=============================*/
@@ -22,79 +33,93 @@ const footer = document.querySelector('footer');
 /*Projects page related*/
 let views = document.querySelectorAll('.view');
 let viewDescriptions = document.querySelectorAll('.viewDescription');
+let gitLinks = document.querySelectorAll('.git-links');
 
 /*CONTACT page related*/
 let inputs = document.querySelectorAll('input');
 const contactLinks = document.querySelectorAll('.contactLink');
 
-if(screen.width > 992){
-    /*=============================PROJECTS-PAGE functions=============================*/
-    /*PROJECT-VIEWS hover functions*/
-    /*showing view descipritons on PROJECT page after hovering PROJECT-VIEW*/
-    for(let i = 0; i < views.length; i++){
-        views[i].addEventListener('mouseover', function(){
-            viewDescriptions[i].classList.add('showDescription');
-        })
-    }
-    for(let i = 0; i < views.length; i++){
-        views[i].addEventListener('mouseout', function(){
-            viewDescriptions[i].classList.remove('showDescription');
-        })
-    }
 
-    /*loading views - PROJECTS page*/
-    for(let i = 0; i < views.length; i++){
-        setTimeout(() => {
-            views[i].style.opacity = "100";
-        },
-        timer);
-        timer -= 300;
-    }
-
-    for(let i = 0; i < inputs.length; i++){
-        setTimeout(() => {
-            inputs[i].style.opacity = "100";
-        },
-        projectTimer);
-        projectTimer -= 250;
-    }
-    /*=============================CONTACT-PAGE functions=============================*/
-    /*loading CONTACT FORM contact.html*/
-    for(let i = 0; i < contactLinks.length; i++){
-        setTimeout(() => {
-            contactLinks[i].style.opacity = "100";
-        },
-        contactLinksTimer);
-        contactLinksTimer -= 200;
-    }
-    setTimeout(() => {
+if(document.title == "Projects"){
+    if(screen.width > 992){
+        /*=============================PROJECTS-PAGE functions=============================*/
+        /*PROJECT-VIEWS hover functions*/
+        /*showing view descipritons on PROJECT page after hovering PROJECT-VIEW*/
+        for(let i = 0; i < views.length; i++){
+            views[i].addEventListener('mouseover', function(){
+                viewDescriptions[i].classList.add('showDescription');
+                gitLinks[i].classList.add('showDescription');
+            })
+        }
+        for(let i = 0; i < views.length; i++){
+            views[i].addEventListener('mouseout', function(){
+                viewDescriptions[i].classList.remove('showDescription');
+                gitLinks[i].classList.remove('showDescription');
+            })
+        }
+        
+        /*loading views - PROJECTS page*/
+        for(let i = 0; i < views.length; i++){
+            setTimeout(() => {
+                views[i].style.opacity = "100";
+            },
+            timer);
+            timer -= 300;
+        }
+        for(let i = 0; i < inputs.length; i++){
+            setTimeout(() => {
+                inputs[i].style.opacity = "100";
+            },
+            projectTimer);
+            projectTimer -= 250;
+        }
+    }else{
+        for(let i = 0; i < views.length; i++){
+            views[i].style.opacity = "100%";
+        }
         footer.style.opacity = "100%";
-    },
-    750);
-
-    setTimeout(() => {
-    document.querySelector('textarea').style.opacity = "100";
     }
-    ,2400);
-    setTimeout(() => {
-        document.querySelector('button').style.opacity = "100";
-    },
-    2200);
+}else  if(document.title == "Contact"){
+    if(screen.width > 992){
+        /*=============================CONTACT-PAGE functions=============================*/
+        /*loading CONTACT FORM contact.html*/
+        for(let i = 0; i < inputs.length; i++){
+            setTimeout(() => {
+                inputs[i].style.opacity = "100";
+            },
+            timer);
+        }
+        for(let i = 0; i < contactLinks.length; i++){
+            setTimeout(() => {
+                contactLinks[i].style.opacity = "100";
+            },
+            contactLinksTimer);
+            contactLinksTimer -= 200;
+        }
+        setTimeout(() => {
+            footer.style.opacity = "100%";
+        },
+        750);
+        setTimeout(() => {
+        document.querySelector('textarea').style.opacity = "100";
+        }
+        ,2400);
+        setTimeout(() => {
+            document.querySelector('button').style.opacity = "100";
+        },
+        2200);
+    }else{
+        for(let i = 0; i < inputs.length; i++){
+            inputs[i].style.opacity = "100%";
+        }
+        for(let i = 0; i < contactLinks.length; i++){
+            contactLinks[i].style.opacity = "100%";
+        }
+        document.querySelector('textarea').style.opacity = "100%";
+        document.querySelector('button').style.opacity = "100%";
+        footer.style.opacity = "100%";
+    }
 }
-else{
-    for(let i = 0; i < views.length; i++){
-        views[i].style.opacity = "100%";
-    }
-    for(let i = 0; i < inputs.length; i++){
-        inputs[i].style.opacity = "100%";
-    }
-    for(let i = 0; i < contactLinks.length; i++){
-        contactLinks[i].style.opacity = "100%";
-    }
-    document.querySelector('textarea').style.opacity = "100%";
-    document.querySelector('button').style.opacity = "100%";
 
-    footer.style.opacity = "100%";
-}
 
 
